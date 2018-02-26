6373b97daae5da0e02e8fab7d84bd2da1b720b90
porzingod <jasonlim.ny@gmail.com>
2018-02-26 11:25:13 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -9,9 +9,17 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    student = Student.create(student_params)
+    redirect_to student_path(student)
+  end
+
+  private
+  def student_params
+    params.permit(:first_name, :last_name)
   end
 
 end
